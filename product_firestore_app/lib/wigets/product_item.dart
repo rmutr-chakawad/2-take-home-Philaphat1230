523bf086cdd943d7e7267314c738e3b3d8d1c865
philaphat1230 <3651051541175@rmutr.ac.th>
2025-02-19 22:58:31 +0700

    ส่งงานครับ

---
+++ product_firestore_app/lib/wigets/product_item.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:product_firestore_app/models/product_model.dart';
+import 'package:product_firestore_app/wigets/product_form.dart';
+import 'package:product_firestore_app/wigets/product_popup.dart';
+
+// ignore: must_be_immutable
+class ProductItem extends StatelessWidget {
+  ProductModel product;
+  ProductItem({super.key, required this.product});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: (){
+        showModalBottomSheet(
+          context: context, 
+          builder: (context){
+            return ProductPopup(product: product);
+          });
+      },
+      child: ListTile(
+        leading: Text(product.productName,),
+        title: Text('${product.price.toStringAsFixed(2)} Bath', textAlign: TextAlign.right,),
+        trailing: IconButton(
+          onPressed: (){
+            showModalBottomSheet(context: context,
+             builder: (context) => ProductForm(product: product,));
+          }, 
+          icon: const Icon(Icons.chevron_right)),
+      
+      ),
+    );
+  }
+}
